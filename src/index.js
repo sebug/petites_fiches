2af6b65464100c0aec3e7d616ec059aea796659e
sebug <sebastian.gfeller@gmail.com>
2018-06-19 15:34:21 +0200

    With apply bindings

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import ko from 'knockout';
 import appRoot from './components/app-root/component.js';
-console.log('webpack works.');
-console.log(ko);
+ko.applyBindings({}, document.getElementByTagName('main'));
+
