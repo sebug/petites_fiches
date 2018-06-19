c15fd24140dc4ab9c28737ea6f6bffce3e111ea7
sebug <sebastian.gfeller@gmail.com>
2018-06-19 15:37:54 +0200

    Correct get element

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ko from 'knockout';
 import appRoot from './components/app-root/component.js';
 ko.components.register('app-root', appRoot);
-ko.applyBindings({}, document.getElementByTagName('main'));
+ko.applyBindings({}, document.getElementsByTagName('main')[0]);
 
